a8ff5a9e33cb18bbd0a463776368d4854b56f923
Sil_Palaoro <silvinapalaoro@gmail.com>
2026-08-17 19:40:31 -0300

    Agrega diagnósticos de version, plataforma, argumentos y variables de entorno

--- diagnostico.js
+++ diagnostico.js
@@ -1,4 +1,26 @@
+//Diagnóstico de la version de Node
 const { version } = require('node:process');
 
 console.log(`Version: ${version}`);
-// Version: v14.8.0
+
+
+//Diagnóstico de la plataforma
+const { platform } = require('node:process');
+
+console.log(`This platform is ${platform}`);
+
+
+//Diagnóstico de argumentos
+const { argv } = require('node:process');
+
+// print process.argv
+argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+
+//Diagnóstico de variables de entorno
+const { env } = require('node:process');
+
+env.TEST="este-es-un-test";
+console.log(env.TEST);
